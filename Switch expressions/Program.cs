f3d8648117f35cae4ce3458d3d7b26b4a1d5c9ec
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-06 20:01:56 +0300

    Add project files.

---
+++ Switch expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Switch_expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;// 1 to 4
+
+            string result;
+
+            result = a switch
+            {
+                1=>"customer",
+                2=>"Client," ,
+                3=>"supplier",
+                4 =>"Distributer",
+                _=>"Invalid"
+
+            };
+
+            Console.WriteLine(result);
+        }
+    }
+}
